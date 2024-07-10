558451ccb1fdf20c181fe69887bd32302ed5cdcf
rivka-yuliia <juliacreadora@gmail.com>
2024-07-10 16:34:48 +0300

    code to separate property

--- main.py
+++ main.py
@@ -7,11 +7,14 @@ class AppException(Enum):
         member = object.__new__(cls)
 
         member._value_ = code
-        member.code = code
         member.exception = type_
         member.message = message
 
         return member
+
+    @property
+    def code(self):
+        return self.value
 
     def throw(self, msg=None):
         if msg is None:
